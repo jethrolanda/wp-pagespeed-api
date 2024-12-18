d5557680d2e36082a5e369a90bfa746338b7746c
Jethro Landa <jemaq_lan@hotmail.com>
2024-12-18 18:49:59 +0800

    initial commit

---
+++ fuel-logic-api.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Plugin Name: Fuel logic API
+ * Description: FL API.
+ * Version: 1.0
+ * Author: Xammis
+ * Author URI: https://xammis.com/
+ * Text Domain: fuel-logic-api
+ * Domain Path: /languages/
+ * Requires at least: 5.7
+ * Requires PHP: 7.2
+ */
+
+defined('ABSPATH') || exit;
+
+add_action('rest_api_init', 'register_testimonial_rest_route');
+
+function register_testimonial_rest_route()
+{
+  register_rest_route(
+    'wp/v2',
+    '/cpts',
+    array(
+      'methods' => 'GET',
+      'callback' => 'get_items',
+    )
+  );
+}
+
+function get_items()
+{
+  $post_types = get_post_types(array('public' => true));
+  $delist = array('page', 'post', 'attachment');
+
+  // remove post types
+  $arr_final = array_diff($post_types, $delist);
+
+  $testimonials = array();
+  $args = array(
+    'post_type' => $arr_final,
+    // 'nopaging' => true,
+  );
+  $query = new WP_Query($args);
+  if ($query->have_posts()) {
+    while ($query->have_posts()) {
+      $query->the_post();
+      $testimonial_data = array(
+        'link' => get_permalink(),
+        // Add other fields as needed
+      );
+      $testimonials[] = $testimonial_data;
+    }
+    wp_reset_postdata();
+  }
+  return rest_ensure_response($testimonials);
+}
